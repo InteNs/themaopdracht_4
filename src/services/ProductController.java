9b15ea661cad08f0ccabc492976a86097a75e9e0
oussamaaalili <mr-aalili@live.nl>
2015-06-18 14:30:48 +0200

    probleem met aanpassen

--- src/services/ProductController.java
+++ src/services/ProductController.java
@@ -14,12 +14,13 @@ import java.util.Objects;
 public class ProductController implements Serializable {
     private ArrayList<Product> products;
 
-    public ProductController(){
+    public ProductController() {
         products = new ArrayList<>();
     }
 
     /**
      * Create new product
+     *
      * @param name
      * @param amount
      * @param price
@@ -27,34 +28,33 @@ public class ProductController implements Serializable {
     public void addProduct(String name, int amount, double price) throws ValidateException {
         boolean succes = true;
         String ERROR_NULL = "Dit veld mag niet leeg zijn!";
-        HashMap<String,String> errorMap = new HashMap<>();
-        if(productExists(name)) {
+        HashMap<String, String> errorMap = new HashMap<>();
+        if (productExists(name)) {
             succes = false;
 
-            errorMap.put("name_error","Deze naam bestaat al in ons systeem!");
+            errorMap.put("name_error", "Deze naam bestaat al in ons systeem!");
         }
-        if(Objects.equals(name, "")) {
+        if (Objects.equals(name, "")) {
             succes = false;
-            errorMap.put("name_error",ERROR_NULL);
+            errorMap.put("name_error", ERROR_NULL);
         }
-        if(Objects.equals(amount, "")) {
+        if (Objects.equals(amount, "")) {
             succes = false;
-            errorMap.put("amount_error",ERROR_NULL);
+            errorMap.put("amount_error", ERROR_NULL);
         }
-        if(Objects.equals(price, "")) {
+        if (Objects.equals(price, "")) {
             succes = false;
-            errorMap.put("price_error",ERROR_NULL);
+            errorMap.put("price_error", ERROR_NULL);
         }
-        if(succes) {
+        if (succes) {
             products.add(new Product(name, amount, price));
             System.out.println("hij doet het");
-        }
-        else throw new ValidateException(errorMap);
+        } else throw new ValidateException(errorMap);
 
 
     }
 
-    public boolean productExists(String name){
+    public boolean productExists(String name) {
         return products.stream().anyMatch(product -> product.getName().equals(name));
     }
 
@@ -64,9 +64,14 @@ public class ProductController implements Serializable {
     }
 
     public Product findProduct(String nameProduct) {
-        for(Product product : products)
-            if(product.getName().equals(nameProduct)) return product;
+        for (Product product : products)
+            if (product.getName().equals(nameProduct)) return product;
         return null;
     }
 
+    public void ammendProduct(String productName, int amount, double price) {
+
+    }
 }
+
+
