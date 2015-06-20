c28f5cf7c9ed56101969205d652e5942871cdcec
InteNs <mark.havekes@gmail.com>
2015-06-20 19:12:37 +0200

    added usercontrollertest empty file re-enabled security filter

---
+++ tests/services/UserControllerTest.java
@@ -0,0 +1,79 @@
+package services;
+
+import org.junit.Test;
+
+/**
+ * Created by InteNs on 17.jun.2015.
+ */
+public class UserControllerTest {
+
+    @Test
+    public void testGetUsers() throws Exception {
+
+    }
+
+    @Test
+    public void testFindUser() throws Exception {
+
+    }
+
+    @Test
+    public void testGetExpiredMaintenanceList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetExpiredVisitList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBlackListedList() throws Exception {
+
+    }
+
+    @Test
+    public void testIsLoginValid() throws Exception {
+
+    }
+
+    @Test
+    public void testUserExists() throws Exception {
+
+    }
+
+    @Test
+    public void testChangeUserInfo() throws Exception {
+
+    }
+
+    @Test
+    public void testNewAdmin() throws Exception {
+
+    }
+
+    @Test
+    public void testNewCustomer() throws Exception {
+
+    }
+
+    @Test
+    public void testNewMechanic() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveUser() throws Exception {
+
+    }
+
+    @Test
+    public void testNewCar() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveCar() throws Exception {
+
+    }
+}
