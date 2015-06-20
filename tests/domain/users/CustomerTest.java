7fd27ee017f3c72823123ad741d82abfdd08f4cd
taoufik321 <hamid030@live.nl>
2015-06-20 21:34:02 +0200

    CustomerTest.java gemaakt

--- tests/domain/users/CustomerTest.java
+++ tests/domain/users/CustomerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by InteNs on 16.jun.2015.
  */
@@ -13,52 +15,56 @@ public class CustomerTest {
 
     @Before
     public void setUp() throws Exception {
-        customer = new Customer("","","", LocalDate.of(1994,3,31),"","","");
+        customer = new Customer("a.a@live.nl","password","joop", LocalDate.of(1994,3,31),"adres","3525RR","0612345678");
     }
 
     @Test
     public void testSetLastMaintenance() throws Exception {
-
+        customer.setLastMaintenance(LocalDate.now());
+        assertEquals("datum komt niet overeen", customer.getLastMaintenance());
     }
 
     @Test
     public void testGetLastVisit() throws Exception {
-
+        assertEquals("datum is niet juist ingesteld", customer.getLastVisit());
     }
 
     @Test
     public void testIsOnBlackList() throws Exception {
-
+        assertEquals("Blacklist is niet juist ingesteld", customer.isOnBlackList());
     }
 
     @Test
     public void testGetLastMaintenance() throws Exception {
-
+        assertEquals("datum komt niet overeen", customer.getLastMaintenance());
     }
 
     @Test
     public void testGetCars() throws Exception {
-
+        assertEquals("Auto-object is niet juist", customer.getCars());
     }
 
     @Test
     public void testAddCar() throws Exception {
-
+        assertEquals("Auto-object is toegevoegd", customer.getCars());
     }
 
     @Test
     public void testRemoveCar() throws Exception {
-
+        customer.removeCar("a");
+        assertEquals("Auto-object is niet verwijderd", customer.getCars());
     }
 
     @Test
     public void testSetLastVisit() throws Exception {
-
+        customer.setLastVisit(LocalDate.now());
+        assertEquals("Laatste bezoek is niet juist ingesteld", customer.getLastVisit());
     }
 
     @Test
     public void testSetIsOnBlackList() throws Exception {
-
+        customer.setIsOnBlackList(true);
+        assertEquals("Blacklist is niet juist ingesteld", customer.isOnBlackList());
     }
 }
 
